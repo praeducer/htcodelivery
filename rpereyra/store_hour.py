712a372024d6bdb9241d2b50fc6f54019ed25571
Paul Prae <paul.prae@outlook.com>
2015-04-01 14:33:35 -0400

    Update store_hour.py

--- rpereyra/store_hour.py
+++ rpereyra/store_hour.py
@@ -1,3 +1,8 @@
+# This is a copy of https://github.com/praeducer/htcodelivery/blob/master/spark/mr_store_hour_local.py
+# Original author was Paul Prae (@Praeducer)
+# Modified for neighborhoods instead of stores by Renato Pereyra (@rpereyra)
+#############################################################
+
 # Mapper
 import string
 from time import strptime
@@ -7,7 +12,7 @@ from collections import Counter
 
 # Mapper
 #########
-# Input: a single record
+# Input: a single record plus neighborhood
 # customer|tier|homeStore|homeStoreLat|homeStoreLon|distance|receipt|datetime|onlineFlag|store|storeLat|storeLon|upc|desc|mupc|subcat|subcat_num|cat|cat_num|dept|dept_num|sales|discount|quantity
 # e.g. bdddad32053f7f53031d5dbfdc0692a01837373f|3|66|35.059823|-80.816172|0.8398516561798964|dd4b97177ebb1ea7b6732cc585ac8d0992ae1f87|2014-09-11 21:04:00|0|11|35.053394|-80.848528|00000000008070|24PK RECYCLING FEE|807|REGULAR|55|CARBONATED BEVERAGES|8|BEVERAGE|23|2.0|0.0|1
 def mapper(record):
